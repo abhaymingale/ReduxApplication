1783626bc4e020d9ad0baed982a68a2b2a46ee80
Abhay Ingale <abhaymingale@gmail.com>
2021-09-13 18:01:02 +0530

    first commit

---
+++ src/component/Homecomponent.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Homecomponent() {
+    return (
+        <div>
+            <h1>Home Component</h1>
+            <div className="cart-wrapper">
+                <img src="" />
+
+             
+            </div>
+        </div>
+    )
+}
+
+
+
+export default Homecomponent
+
---
+++ src/container/Homecontainer.js
@@ -0,0 +1,4 @@
+import Homecontainer  from "../component/Homecomponent";
+
+
+export default Homecontainer
